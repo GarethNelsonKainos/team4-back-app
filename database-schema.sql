16fc3208555b038c290b2dde87c6f497bd35953d
CameronStewartKainos <cameron.stewart@kainos.com>
2026-02-09 11:44:47 +0000

    Remove unneccessary notnulls and location name change

--- database-schema.sql
+++ database-schema.sql
@@ -3,23 +3,23 @@
 -- Create Band table
 CREATE TABLE Band (
     bandId SERIAL PRIMARY KEY,
-    bandName VARCHAR(100) NOT NULL
+    bandName VARCHAR(100)
 );
 
 -- Create Capability table
 CREATE TABLE Capability (
     capabilityId SERIAL PRIMARY KEY,
-    capabilityName VARCHAR(100) NOT NULL
+    capabilityName VARCHAR(100)
 );
 
 -- Create Job-roles table
 CREATE TABLE JobRoles (
     jobRoleId SERIAL PRIMARY KEY,
-    roleName VARCHAR(200) NOT NULL,
-    location VARCHAR(100) NOT NULL,
+    roleName VARCHAR(200),
+    jobLocation VARCHAR(100),
     capabilityId INT NOT NULL,
     bandId INT NOT NULL,
-    closingDate DATE NOT NULL,
+    closingDate DATE,
     FOREIGN KEY (capabilityId) REFERENCES Capability(capabilityId),
     FOREIGN KEY (bandId) REFERENCES Band(bandId)
 );
