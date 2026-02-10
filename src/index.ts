7fb710d3a20d02ad24e6646f2d680b9dd658e2fa
CameronStewartKainos <cameron.stewart@kainos.com>
2026-02-10 11:49:08 +0000

    fixing biome errors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-import { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 
 const app = express();
 const port = 3000
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello world!');
+app.get("/", (_req: Request, res: Response, _next: NextFunction) => {
+    res.send("Hello world!");
 });
 
 app.listen(port, () => {
