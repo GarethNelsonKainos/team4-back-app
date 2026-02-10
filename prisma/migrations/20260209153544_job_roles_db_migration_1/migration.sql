09feb4268ffb23433312c1982dbc8b6fb62ddc1c
CameronStewartKainos <cameron.stewart@kainos.com>
2026-02-10 09:17:46 +0000

    working prisma

---
+++ prisma/migrations/20260209153544_job_roles_db_migration_1/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "Band" (
+    "bandId" SERIAL NOT NULL,
+    "bandName" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "Band_pkey" PRIMARY KEY ("bandId")
+);
+
+-- CreateTable
+CREATE TABLE "Capability" (
+    "capabilityId" SERIAL NOT NULL,
+    "capabilityName" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "Capability_pkey" PRIMARY KEY ("capabilityId")
+);
+
+-- CreateTable
+CREATE TABLE "JobRoles" (
+    "jobRoleId" SERIAL NOT NULL,
+    "roleName" VARCHAR(200) NOT NULL,
+    "location" VARCHAR(100) NOT NULL,
+    "capabilityId" INTEGER NOT NULL,
+    "bandId" INTEGER NOT NULL,
+    "closingDate" DATE NOT NULL,
+
+    CONSTRAINT "JobRoles_pkey" PRIMARY KEY ("jobRoleId")
+);
+
+-- AddForeignKey
+ALTER TABLE "JobRoles" ADD CONSTRAINT "JobRoles_capabilityId_fkey" FOREIGN KEY ("capabilityId") REFERENCES "Capability"("capabilityId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "JobRoles" ADD CONSTRAINT "JobRoles_bandId_fkey" FOREIGN KEY ("bandId") REFERENCES "Band"("bandId") ON DELETE RESTRICT ON UPDATE CASCADE;
