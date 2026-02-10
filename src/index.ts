f4f633a5fa1dd5ed374da6ad4d46e97c44ff466b
CameronStewartKainos <cameron.stewart@kainos.com>
2026-02-10 11:53:33 +0000

    fixed more biome errors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import type { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from "express";
+import express from "express";
 
 const app = express();
-const port = 3000
+const port = 3000;
 
 app.use(express.json());
 
 app.get("/", (_req: Request, res: Response, _next: NextFunction) => {
-    res.send("Hello world!");
+	res.send("Hello world!");
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+	console.log(`App listening on port ${port}`);
+});
